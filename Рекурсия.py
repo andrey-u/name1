b88ffaa240e7b7fcfeabf83cc91cb2699f0288a9
andrey-u <72195241+andrey-u@users.noreply.github.com>
2020-10-19 13:34:34 +0300

    Рекурсия2.py

--- Рекурсия.py
+++ Рекурсия.py
@@ -1,7 +1,7 @@
 from os import walk
 
 def rec(dir):
-    for (dirpath, dirnames, files) in walk(dir):
+    for (somth, dirnames, files) in walk(dir):
         for file in files:
             print(file)
         if len(dirnames) != 0:
